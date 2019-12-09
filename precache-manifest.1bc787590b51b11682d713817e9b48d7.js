66cad37c6484891a49df0ca124e52c88536e0e0b
Vvxforce <vvxforce@gmail.com>
2019-12-09 17:24:51 +0200

    Updates

---
+++ precache-manifest.1bc787590b51b11682d713817e9b48d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b099f3d5b508474ead208d87e0c6c67",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "be338af385d1b5cb8099",
+    "url": "/react_tabs/static/css/main.0d28cf7d.chunk.css"
+  },
+  {
+    "revision": "477d3cfdba83b1e0f343",
+    "url": "/react_tabs/static/js/2.345f8e30.chunk.js"
+  },
+  {
+    "revision": "be338af385d1b5cb8099",
+    "url": "/react_tabs/static/js/main.60ed73af.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
